3a21f45381574d5f387b99c65551d9393fdc9b18
Fatan Akbar Yogenta <fatan.dev@gmail.com>
2024-07-25 10:07:49 +0700

    asd

--- viewPhotosHelloTeman.py
+++ viewPhotosHelloTeman.py
@@ -93,4 +93,4 @@ def show_photos():
     return render_template_string(html_template, photos=photos_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
