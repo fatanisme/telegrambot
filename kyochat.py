2b232047e885ec372cacfe4698f209e7bd5120f5
Fatan Akbar Yogenta <fatan.dev@gmail.com>
2024-08-19 22:23:58 +0700

    qweasd

--- kyochat.py
+++ kyochat.py
@@ -135,7 +135,7 @@ async def send_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE)-> N
         ["🙎‍♂️🙎‍♀️ Cari berdasarkan Jenis Kelamin 🙎‍♂️🙎‍♀️"],
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Pilih opsi:", reply_markup=reply_markup)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="-----------", reply_markup=reply_markup)
 
 async def remove_keyboard(update, context):
     reply_markup = ReplyKeyboardRemove()
